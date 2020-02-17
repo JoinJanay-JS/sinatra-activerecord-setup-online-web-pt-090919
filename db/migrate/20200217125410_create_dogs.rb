0cb6f9f02c238f037f49906f6f8174e672fd01a3
Janay Bailey <tjanay87@gmail.com>
2020-02-17 12:55:13 +0000

    Automatically backed up by Learn

--- db/migrate/20200217125410_create_dogs.rb
+++ db/migrate/20200217125410_create_dogs.rb
@@ -1,4 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change
-  end
+   def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end 
+  
+  
 end
